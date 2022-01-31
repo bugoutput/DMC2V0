afeb2d8243d17c45708dfe923e078290e10a374a
Justin Ball <37853570+bugoutput@users.noreply.github.com>
2022-01-31 01:26:20 -0700

    Update makeV0.py

--- makeV0.py
+++ makeV0.py
@@ -6,7 +6,7 @@ client = Client("IRIS")
 
 # Select your desired station to create V0 files for here:
 network="RE"
-station="JKLK2"
+station="JKLK"
 
 # Grab channel metadata from IRIS:
 inventory = client.get_stations(network=network, station=station, level="channel")
@@ -21,14 +21,32 @@ for n in chans:
     
     # Channel parameters being written to the newly-created V0 files:
     tdata = {
-        "lat": float("%.3f" % md['latitude']),
-        "lon": float("%.3f" % md['longitude']),
-        "az": md['azimuth'],
+        "latr": float("%.3f" % md['latitude']),
+        "lonr": float("%.3f" % md['longitude']),
+        "lat": float("%.6f" % md['latitude']),
+        "lon": float("%.6f" % md['longitude']),
+        "elev": md['elevation'],
+        "az": int(md['azimuth']),
         "nc": network,
         "sta": station,
         "ch": scnl[3],
         "chn": chnum+1,
-        "lc": scnl[2]
+        "lc": scnl[2],
+        "netnum": 3, # network code, use 3 for USBR
+        "recsn": -999, # recorder serial number
+        "rectype": 0, # recorder type, use 1000 for "other"
+        "senstype": 900, # sensor type, use 900 for "other"
+        "statype": 12, # station type, use 12 for "dam" or 1 for freefield
+        "units": 50, # units, use 50 for counts
+        "numchan": len(chans), # number of channels in recorder
+        "rLSB": -999.00, # uV per count (LSB in microvolts)
+        "vpp": 40, # recorder full scale input (volts)
+        "wordlen": 0, # sample word length
+        "corner": -999, # anti-alias filter corner freq (Hz)
+        "sens": 15, # sensitivity (volts/g)
+        "spp": 60.0000, # full scale output of sensor (volts)
+        "sg": 4, # full scale range of sensor (g)
+        "gain": -999 # sensor gain factor
         }
     
     # COSMOS V0 template being modified and written per each channel: 
@@ -38,7 +56,7 @@ for n in chans:
     Hypocenter:  TBD                H=   km                                         
     Origin:  TBD                                                                    
     Statn No: 06-000000 Code:{{nc}}-{{sta}}        Calipatria 2                            
-    Coords: {{lat}}  {{lon}}    Site Geology:                                        
+    Coords: {{latr}}  {{lonr}}    Site Geology:                                        
     Recorder:       Deg           Unk  s/n  1114   ( 3 Chns of   6 at Sta)   Sensor:   
     Rcrd start time:06/17/2018, 18:34:36.000 GMT (Q=5) RcrdId:38199368.CI.CLI2.--.H 
     Sta Chan   {{chn}}  {{az}}    {{ch}}           Location:   {{lc}}
@@ -47,26 +65,26 @@ for n in chans:
     No filtering!                                                                   
     Values used when parameter of data value is unknown/unspecified:  -999, -999.000
      100 Integer-header values follow on  10 lines, Format = (10I8)                 
-           0       1      50     120       1    -999    -999       0    -999    -999
-           6       6    -999       6    -999       1    -999    -999       3    -999
-        -999    -999       6    -999    -999    -999    -999    -999    -999       0
-           3    1114       6       3       0    -999    -999    -999    -999    2018
-         168       6      17      18      34       5       5    -999    -999       1
-           1      20    -999      90    -999    -999    -999    -999    -999    -999
+           0       1      {{units}}     120       1    -999    -999       0    -999    -999
+           {{netnum}}       {{netnum}}    -999       {{netnum}}    -999       1    -999    -999      {{statype}}    -999
+        -999    -999       {{numchan}}    -999    -999    -999    -999    -999    -999       {{rectype}}
+           3    {{recsn}}       {{numchan}}       {{numchan}}       {{wordlen}}    -999    -999    -999    -999    2018
+         168       6      17      18      34       5       5    -999    -999       {{chn}}
+           {{chn}}     {{senstype}}    -999       {{az}}    -999      {{lc}}    -999    -999    -999    -999
         -999    -999    -999    -999    -999    -999    -999    -999    -999    -999
         -999    -999    -999    -999       1    -999    -999    -999    -999    -999
        17770     100      18      34      36       0       6      17    2018    -999
          360    -999       3    -999    -999    -999       0    -999    -999    -999
      100 Real-header values follow on  20 lines, Format =(5F15.6)
-          33.121120    -115.580162     -60.200001    -999.000000    -999.000000
+           {{lat}}     {{lon}}         {{elev}}    -999.000000    -999.000000
         -999.000000    -999.000000    -999.000000    -999.000000    -999.000000
         -999.000000    -999.000000    -999.000000    -999.000000    -999.000000
         -999.000000    -999.000000    -999.000000    -999.000000    -999.000000
-        -999.000000       0.596047       0.000000    -999.000000    -999.000000
-        -999.000000    -999.000000    -999.000000    -999.000000      36.000000
+        -999.000000         {{rLSB}}             {{vpp}}    -999.000000    -999.000000
+        -999.000000    -999.000000    -999.000000    -999.000000             36
         -999.000000    -999.000000    -999.000000       0.010000     177.699997
         -999.000000    -999.000000    -999.000000    -999.000000     200.000000
-           0.707100       4.995260      20.000000       4.000000    -999.000000
+           0.707100             {{sens}}           {{spp}}              {{sg}}    -999.000000
         -999.000000       1.000000    -999.000000    -999.000000    -999.000000
         -999.000000       0.000000    -999.000000    -999.000000    -999.000000
         -999.000000    -999.000000    -999.000000    -999.000000    -999.000000
@@ -87,7 +105,10 @@ for n in chans:
     v0temp = Template(template)
     outtext = v0temp.render(tdata)
     print(v0temp.render(tdata))
-    fname = chans[chnum] + ".dlv0"
+    # fname = chans[chnum] + ".dlv0"
+    foo = chans[chnum].split('.')
+    fname = network + "_" + foo[1] + "_" + foo[2] + "_" + scnl[3] + ".dlv0"
     with open(fname, 'w') as f:
         f.write(outtext)
     chnum+=1
+    
